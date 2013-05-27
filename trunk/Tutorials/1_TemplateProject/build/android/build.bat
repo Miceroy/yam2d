f7ec7c0b14c90f140e954f58ea3cb8606a476142
kajakbros@gmail.com <kajakbros@gmail.com@2756e8b1-e213-d811-d314-e58f43a541ef>
2013-05-27 12:52:19 +0000

    Updates for first working Example for Android.

---
+++ trunk/Tutorials/1_TemplateProject/build/android/build.bat
@@ -0,0 +1,10 @@
+set NDK_ROOT=C:\work\android-ndk-r8e\
+rem set ROOT=%cd%
+rem echo Root folder %ROOT%
+
+
+call %NDK_ROOT%ndk-build.cmd
+
+
+pause
+
---
+++ trunk/Tutorials/1_TemplateProject/build/android/rebuild.bat
@@ -0,0 +1,10 @@
+set NDK_ROOT=C:\work\android-ndk-r8e\
+
+
+
+call %NDK_ROOT%ndk-build.cmd clean
+
+call %NDK_ROOT%ndk-build.cmd
+
+pause
+
