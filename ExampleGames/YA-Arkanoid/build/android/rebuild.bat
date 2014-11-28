2c8622777ad906f95c3be54c64e0fc1b52233363
kajakbros@gmail.com <kajakbros@gmail.com@2756e8b1-e213-d811-d314-e58f43a541ef>
2014-11-28 10:50:31 +0000

    Updated Android ndk version to r10c.

--- ExampleGames/YA-Arkanoid/build/android/rebuild.bat
+++ ExampleGames/YA-Arkanoid/build/android/rebuild.bat
@@ -1,4 +1,4 @@
-set NDK_ROOT=C:\work\android-ndk-r9c\
+set NDK_ROOT=C:\work\android-ndk-r10c\
 
 xcopy ..\..\assets\* assets\*  /Y
 
--- Tutorials/1_TemplateProject/build/android/build.bat
+++ Tutorials/1_TemplateProject/build/android/build.bat
@@ -1,5 +1,7 @@
-set NDK_ROOT=C:\work\android-ndk-r9c\
+set NDK_ROOT=C:\work\android-ndk-r10c\
 
+
+xcopy ..\..\assets\* assets\*  /Y
 
 call %NDK_ROOT%ndk-build.cmd
 
